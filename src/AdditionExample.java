f2b5596ee886962954dcb8a4295b2fa16c9f2027
kena2606 <kena.acharya26@gmail.com>
2020-06-07 16:29:52 +0100

    steps

---
+++ src/AdditionExample.java
@@ -0,0 +1,21 @@
+public class AdditionExample {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int addition = a+b;
+
+        System.out.println("Total of A and B is: " + addition);
+        int x=30;
+        int y=20;
+        int substraction =x-y;
+
+        System.out.println("Substraction of x-y is: "  + substraction);
+
+        int j= 30;
+        int k= 50;
+
+        System.out.println("Multiplication of j and k is: "+ j*k);
+
+
+    }
+}
